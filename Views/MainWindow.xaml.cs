a492ccc1f9717d65dcb0b0ba1322e475fce80d80
lZiMUl <AA2908554069@gmail.com>
2024-05-24 13:58:57 -0700

    Update

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,21 +1,33 @@
-﻿using System.Windows;
-using System.Windows.Media.Imaging;
+﻿using BiliBili_Anchor_Assistant.Tools;
+using System.Windows;
 
 namespace BiliBili_Anchor_Assistant.Views
 {
     public partial class MainWindow
     {
-        // private static string url = "https://cn.bing.com";
+        private static readonly UriBuilder Uri = new("https://cn.bing.com");
+        private SongManager? _songManager;
         public MainWindow()
         {
             InitializeComponent();
             Icon = Config.Icon;
         }
-        private void ConnectButton(object sender, RoutedEventArgs e)
+        private async void ConnectButton(object sender, RoutedEventArgs e)
         {
-            // string data = await Http.Get(url);
-            new SongManager().ShowDialog();
-            // Console.Out.WriteLine(data);
+            string roomId = RoomID.Text;
+            Uri.Query = "?/6";
+            string data = await Http.Get(Uri.ToString());
+
+            Console.Out.WriteLine(data);
+        }
+        private void SongManager(object sender, RoutedEventArgs e)
+        {
+            _songManager = Application.Current.Windows.OfType<SongManager>().FirstOrDefault() ?? new SongManager();
+            if (!_songManager.IsVisible)
+            {
+                _songManager.Show();
+            }
+            _songManager.Focus();
         }
     }
 }
--- Views/SongManager.xaml.cs
+++ Views/SongManager.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace BiliBili_Anchor_Assistant.Views
 {
@@ -16,15 +17,37 @@ namespace BiliBili_Anchor_Assistant.Views
             var stackPanel = new StackPanel();
             stackPanel.Orientation = Orientation.Horizontal;
 
-            var textBox = new TextBox();
-            textBox.Text = "1";
+            var nameTextBox = new TextBlock { Text = "a", Width = 160 };
+            var authorTextBox = new TextBlock { Text = "b", Width = 160 };
+            var timeTextBox = new TextBlock { Text = "c", Width = 160 };
+            var sizeTextBox = new TextBlock { Text = "d", Width = 160 };
 
-            stackPanel.Children.Add(textBox);
+            var deleteButton = new Button
+            {
+                Content = "Delete",
+                Background = Brushes.OrangeRed,
+                Style = FindResource("ButtonStyle") as Style
+            };
+            deleteButton.Click += DeleteSong;
+
+            stackPanel.Children.Add(nameTextBox);
+            stackPanel.Children.Add(authorTextBox);
+            stackPanel.Children.Add(timeTextBox);
+            stackPanel.Children.Add(sizeTextBox);
+            stackPanel.Children.Add(deleteButton);
+
+            int rowIndex = SongList.RowDefinitions.Count;
+            SongList.RowDefinitions.Add(new RowDefinition());
+            Grid.SetRow(stackPanel, rowIndex + 1);
             SongList.Children.Add(stackPanel);
         }
         private void DeleteSong(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            var button = sender as Button;
+            var stackPanel = button.Parent as StackPanel;
+
+            // 从 SongList 中移除该 StackPanel
+            SongList.Children.Remove(stackPanel);
         }
     }
 }
