590960b10a0785b829828abf189360e9e170de39
lZiMUl <lzimul@lzimul.top>
2024-06-15 04:47:57 -0700

    None:(((((

--- ViewModels/Pages/SongControl.cs
+++ ViewModels/Pages/SongControl.cs
@@ -14,27 +14,44 @@ namespace com.lZiMUl.BiliBili_Anchor_Assistant.ViewModels.Pages
             Next.Source = Config.ImageResource.Next;
         }
 
+
+        // # Bugs 播放器控制功能
         private void TouchDownEvent(object sender, RoutedEventArgs e)
         {
-
-            switch ((sender as Image)?.Name)
+            var image = sender as Image;
+            switch (image?.Name)
             {
                 case "Previous":
+                    PreviousSong();
                     break;
 
                 case "Play":
+                    image.Name = "Pause";
                     break;
 
                 case "Pause":
+                    image.Name = "Play";
                     break;
 
                 case "Next":
+                    NextSong();
                     break;
 
                 default:
 
                     throw new SyntaxErrorException(ToString());
             }
+        }
+
+        private static void PreviousSong()
+        {
+
+        }
+
+
+    private static void NextSong()
+        {
+
         }
     }
 }
