ba2d606f2d09e5339c3c6686ca97d9045b469c54
lZiMUl <lzimul@lzimul.top>
2024-06-15 04:32:04 -0700

    Add Action Event

--- ViewModels/Pages/SongControl.cs
+++ ViewModels/Pages/SongControl.cs
@@ -1,4 +1,8 @@
-﻿namespace com.lZiMUl.BiliBili_Anchor_Assistant.ViewModels.Pages
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace com.lZiMUl.BiliBili_Anchor_Assistant.ViewModels.Pages
 {
     public partial class SongControl
     {
@@ -8,6 +12,29 @@
             Previous.Source = Config.ImageResource.Previous;
             Play.Source = Config.ImageResource.Play;
             Next.Source = Config.ImageResource.Next;
+        }
+
+        private void TouchDownEvent(object sender, RoutedEventArgs e)
+        {
+
+            switch ((sender as Image)?.Name)
+            {
+                case "Previous":
+                    break;
+
+                case "Play":
+                    break;
+
+                case "Pause":
+                    break;
+
+                case "Next":
+                    break;
+
+                default:
+
+                    throw new SyntaxErrorException(ToString());
+            }
         }
     }
 }
